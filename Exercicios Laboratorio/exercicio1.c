ba2feded421cdf676f8d17df2c22b03cc81a4b81
Luiz Haroldo <61132305+LHneto@users.noreply.github.com>
2024-02-22 19:24:00 -0300

    Update exercicio1.c

--- Exercicios Laboratorio/exercicio1.c
+++ Exercicios Laboratorio/exercicio1.c
@@ -3,21 +3,20 @@
 int main() {
     int num1, num2;
 
-    printf("Digite o primeiro número: ");
+    printf("Informe o primeiro número: ");
     scanf("%d", &num1);
 
-    printf("Digite o segundo número: ");
+    printf("Informe o segundo número: ");
     scanf("%d", &num2);
 
     int diff = num1 - num2;
-
-    int Maior = ((diff >> 31) & 1) | ((!!diff) & !(diff & (1 << 31)));
-
-    if (Maior) {
-        printf("O maior número é: %d\n", num1);
-    } else if (diff) {
-        printf("O maior número é: %d\n", num2);
-    } else {
+    
+    if (diff)
+    {
+        printf("O número maior é: %d\n", num1 - (diff & (diff >> (sizeof(int) * 8 - 1))));
+    }
+    else
+    {
         printf("Os números são iguais.\n");
     }
 
